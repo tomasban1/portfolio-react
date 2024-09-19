c62b321e45a4e1d82ef92ba201cc22d2f68d504c
tomasban1 <tomasbanius@gmail.com>
2024-09-19 22:33:57 +0300

    up

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,12 +7,12 @@ export function Hero (){
         <div className={styles.heroContainer}>
             <img style={{width:200, height:200, borderRadius: '100%'}} src={logo} alt="Profile pic" />
             <div className={styles.description}>
-                <h1 style={{width:700, alignSelf: 'center'}}>I'am Tomas Banisevičius, <br/> welcome to my portfolio page!</h1>
+                <h1 style={{width:700, alignSelf: 'center'}}>I'am <span className={styles.name}>Tomas Banisevičius,</span> <br/> welcome to my portfolio page!</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam omnis saepe quae rem blanditiis impedit excepturi deleniti, ut, in dolor quidem suscipit eos atque, libero dolore earum asperiores. Ea, architecto.</p>
             </div>
             <div className={styles.btnContainer}>
-                <button>Contact Me!</button>
-                <button>My resume..</button>
+                <button className={styles.btnContactMe}>Contact Me!</button>
+                <button className={styles.btnResume}>My resume..</button>
             </div>
         </div>
     );
